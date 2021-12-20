594c5aeaa56c1978c591f41b7ea8170d65dd9f1f
Shaikh Rahman <smrahman0009@gmail.com>
2021-12-20 15:31:57 +0600

    Number of provinces solved using union find solutions

--- Number_Of_Provinces_547.py
+++ Number_Of_Provinces_547.py
@@ -1,33 +1,30 @@
 class Solution(object):
-    flag = []
-    # Deepth first search
-    def checkNode(self, isConnected, nodeNo):
-        global flag
-        i = 0
-        while i < len(isConnected[nodeNo]):
-            if isConnected[nodeNo][i] == 1 and flag[i] == 0:
-                flag[i] = 1
-                self.checkNode(isConnected, i)
-                print(isConnected[nodeNo][i])
-            i += 1
-
-        return 1
+    # union find method
+    def findParent(self, parent, node):
+        if parent[node] == -1:
+            return node
+        return self.findParent(parent, parent[node])
 
     def findCircleNum(self, isConnected):
         """
         :type isConnected: List[List[int]]
         :rtype: int
         """
-        global flag
-        flag = [0] * len(isConnected)
-        region = 0
-
-        for nodeNo, node in enumerate(isConnected):
-            if flag[nodeNo] == 0:
-                flag[nodeNo] = 1
-                region += 1
-                self.checkNode(isConnected, nodeNo)
-        print(region)
+        parent = [-1] * len(isConnected)
+        region = len(isConnected)
+        node = 0
+        while node < len(isConnected):
+            path = 0
+            while path < len(isConnected[node]):
+                if isConnected[node][path] == 1 and node != path:
+                    parentNode = self.findParent(parent, node)
+                    parentPath = self.findParent(parent, path)
+                    if parentNode != parentPath:
+                        parent[parentNode] = parentPath
+                        region -= 1
+                path += 1
+            node += 1
+        print(parent)
         return region
 
 
